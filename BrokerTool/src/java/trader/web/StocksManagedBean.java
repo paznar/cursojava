233312411000f33e846e1cb139e94c7aedca53c2
paznar <pablito_azn@hotmail.com>
2015-03-10 22:51:25 +0100

    CustomerDetails.xhtml Stocks.xhtml con JSF

---
+++ BrokerTool/src/java/trader/web/StocksManagedBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trader.web;
+
+import trader.*;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Hyaku Nin Giri
+ */
+@ManagedBean(name = "stocks")
+@RequestScoped
+public class StocksManagedBean {
+
+    private BrokerModel model = BrokerModelImpl.getInstance();
+    /**
+     * Creates a new instance of StocksManagedBean
+     */
+    public StocksManagedBean() {
+    }
+    
+    public Stock[] getAllStocks() {
+        
+        Stock [] stocks = null;
+        try 
+        {
+            stocks = model.getAllStocks();
+        } 
+        catch (BrokerException be) 
+        {
+            return null;
+        }
+        return stocks;
+    }
+    
+}
